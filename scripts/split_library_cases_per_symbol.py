74ce1c80567ec6912e0a92dbc3e501f922f55e16
Jon Ruttan <jonruttan@gmail.com>
2026-02-18 16:34:52 -0500

    Remove additional obsolete script shim entrypoints

--- scripts/split_library_cases_per_symbol.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from __future__ import annotations
-
-from spec_runner.split_library_cases_per_symbol import main
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
