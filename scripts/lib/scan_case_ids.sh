b2bb878c8947eb21b6762738b1f7d64cc91b71e1
Jon Ruttan <jonruttan@gmail.com>
2026-02-20 23:44:21 -0500

    fix governance case-id scan for suite contract lists

--- scripts/lib/scan_case_ids.sh
+++ scripts/lib/scan_case_ids.sh
@@ -2,7 +2,8 @@
 
 scan_case_ids() {
   local root_dir="$1"
-  rg -n '^id:\s*' "${root_dir}/specs" -g '*.spec.md' | sed -E 's/.*id:\s*//'
+  rg -n '^(id:|  - id:)\s*' "${root_dir}/specs" -g '*.spec.md' \
+    | sed -E 's/.*id:\s*//'
 }
 
 count_duplicate_case_ids() {
